d27ca8f6b257585d6427ccc981591737095a08f0
LiuTao <mat.liutos@gmail.com>
2013-07-31 23:01:09 +0800

    实现多个操作符并支持标识符
    1. 实现了对幂乘、阶乘以及赋值操作的转换
    2. 支持任意长度的标识符，使用了string_builder_t类型。

--- poster.c
+++ poster.c
@@ -9,6 +9,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "object.h"
+#include "type.h"
+#include "utilities.h"
+
 typedef struct token_t token_t;
 typedef struct token_vector_t token_vector_t;
 
@@ -17,6 +21,7 @@ enum TOKEN_TYPE {
   OPERATOR,
   LPAREN,
   RPAREN,
+  ID,
 };
 
 enum {
@@ -29,6 +34,7 @@ struct token_t {
   union {
     int number;
     char operator;
+    char *id;
   } u;
 };
 
@@ -46,6 +52,8 @@ int main(int argc, char *argv[]) {
   convert_write("(2 * 3)");
   convert_write("9 - (5 + 2)");
   convert_write("(1 + 2) * 3");
+  convert_write("(1 + 2) ^ 3!");
+  convert_write("var = 1 + 2");
   return 0;
 }
 
@@ -72,6 +80,13 @@ token_t *make_lparen(void) {
 token_t *make_rparen(void) {
   token_t *tk = malloc(sizeof(struct token_t));
   tk->type = RPAREN;
+  return tk;
+}
+
+token_t *make_id(char *id) {
+  token_t *tk = malloc(sizeof(struct token_t));
+  tk->type = ID;
+  tk->u.id = id;
   return tk;
 }
 
@@ -113,6 +128,8 @@ int op_precedence(token_t *op) {
   switch (op->u.operator) {
     case '+': case '-': return 0;
     case '*': case '/': return 1;
+    case '^': case '!': return 2;
+    case '=': return -1;
     default :
       fprintf(stderr, "Unknown operator %c\n", op->u.operator);
       exit(1);
@@ -124,6 +141,8 @@ int op_associate(token_t *op) {
   switch (op->u.operator) {
     case '+': case '-': case '*': case '/':
       return LEFT;
+    case '^': case '!': case '=':
+      return RIGHT;
     default :
       fprintf(stderr, "Unknown operator %c\n", op->u.operator);
       exit(1);
@@ -131,7 +150,7 @@ int op_associate(token_t *op) {
 }
 
 int isoperator(char c) {
-  return c == '+' || c == '-' || c == '*' || c == '/';
+  return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '!' || c == '=';
 }
 
 int is_left_assoc(token_t *op) {
@@ -156,10 +175,10 @@ token_vector_t *infix2postfix(char *str) {
       i++;
     } else if (c == ')') {
       while (!is_vector_empty(stack)) {
-	token_t *tk = pop(stack);
-	if (tk->type == LPAREN)
-	  break;
-	push(tk, queue);
+        token_t *tk = pop(stack);
+        if (tk->type == LPAREN)
+          break;
+        push(tk, queue);
       }
       i++;
     } else if (isdigit(c)) {
@@ -170,23 +189,31 @@ token_vector_t *infix2postfix(char *str) {
         c = str[i];
       } while (isdigit(c));
       push(make_number(n), queue);
+    } else if (isalpha(c)) {
+      string_builder_t *sb = make_str_builder();
+      while (isalpha(c)) {
+        sb_add_char(sb, c);
+        i++;
+        c = str[i];
+      }
+      push(make_id(sb2string(sb)), queue);
     } else if (isoperator(c)) {
       i++;
       token_t *o1 = make_operator(c);
       if (!is_vector_empty(stack)) {
         token_t *o2 = top(stack);
-	if (!isparen(o2)) {
-	  int p1 = op_precedence(o1);
-	  int p2 = op_precedence(o2);
-	  while ((is_left_assoc(o1) && p1 <= p2) ||
-		 (is_right_assoc(o1) && p1 < p2)) {
-	    pop(stack);
-	    push(o2, queue);
-	    if (is_vector_empty(stack))
-	      break;
-	    o2 = top(stack);
-	  }
-	}
+        if (!isparen(o2)) {
+          int p1 = op_precedence(o1);
+          int p2 = op_precedence(o2);
+          while ((is_left_assoc(o1) && p1 <= p2)
+              || (is_right_assoc(o1) && p1 < p2)) {
+            pop(stack);
+            push(o2, queue);
+            if (is_vector_empty(stack))
+              break;
+            o2 = top(stack);
+          }
+        }
       }
       push(o1, stack);
     } else {
@@ -210,11 +237,14 @@ void write_tokens(token_vector_t *vector) {
         printf("%c", tk->u.operator);
         break;
       case LPAREN:
-	printf("(");
-	break;
+        printf("(");
+        break;
       case RPAREN:
-	printf(")");
-	break;
+        printf(")");
+        break;
+      case ID:
+        printf("%s", tk->u.id);
+        break;
     }
   }
 }
