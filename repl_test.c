3e64d50bf5ec49cab95ee204d41f50c3b7874c1d
LiuTao <mat.liutos@gmail.com>
2013-08-09 23:19:59 +0800

    使用getopt进行简单的命令行参数处理，提供脚本化功能。

--- repl_test.c
+++ repl_test.c
@@ -4,6 +4,10 @@
  *  Created on: 2013年7月26日
  *      Author: liutos
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "compiler.h"
 #include "macros.h"
 #include "object.h"
@@ -16,6 +20,23 @@ int main(int argc, char *argv[]) {
   init_prims();
   init_macros();
   load_init_file();
+
+  int opt;
+  while ((opt = getopt(argc, argv, "l:")) != -1) {
+    switch (opt) {
+      case 'l': {
+        char *script = optarg;
+        lt *path = make_string(script);
+        lt_load(path);
+        exit(1);
+      }
+        break;
+      default :
+        fprintf(stderr, "Unknown command line option %c\n", opt);
+        exit(1);
+    }
+  }
+
   while (1) {
     write_raw_string(">> ", standard_out);
     lt *expr = read_object(standard_in);
