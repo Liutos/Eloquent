eca8839b80c113dcfc75e5e5e9af972a8fb9859f
LiuTao <mat.liutos@gmail.com>
2015-07-21 21:51:10 +0800

    生成编译器的REPL

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 CC = gcc
 CFLAGS = -Wall -g -pg
-SRCS = $(shell find . -maxdepth 1 -name '*.c')
+SRCS = $(shell find . -maxdepth 1 -name '*.c' -not -name '*main.c')
 OBJS = $(patsubst %.c, %.o, $(SRCS))
 ELOI = eloi
+ELOC = eloc
 
-all: $(ELOI)
+all: $(ELOI) $(ELOC)
 
 depend: .depend
 
@@ -24,11 +25,15 @@ utils/vector.o: utils/vector.c utils/vector.h
 
 # executables
 
-$(ELOI): $(OBJS) utils/hash_table.o utils/string.o utils/vector.o
+$(ELOI): main.o $(OBJS) utils/hash_table.o utils/string.o utils/vector.o
+	$(CC) $(CFLAGS) -o $@ $^
+
+$(ELOC): cv_main.o $(OBJS) utils/hash_table.o utils/string.o utils/vector.o
 	$(CC) $(CFLAGS) -o $@ $^
 
 .PHONY: clean
 
 clean:
 	if [ -f $(ELOI) ]; then rm -vf $(ELOI); fi
+	if [ -f $(ELOC) ]; then rm -vf $(ELOC); fi
 	find . -name '*.o' -exec rm -rvf {} \;
---
+++ cv_main.c
@@ -0,0 +1,46 @@
+/*
+ * cv_main.c
+ *
+ *  Created on: 2015年7月21日
+ *      Author: liutos
+ */
+#include <stdio.h>
+#include "ast.h"
+#include "bytecode.h"
+#include "compiler.h"
+#include "lexer.h"
+#include "parser.h"
+
+static ast_kind_t prompt(parser_t *parser, ast_t **ast)
+{
+    fprintf(stdout, "233-LISP > ");
+    fflush(stdout);
+    return parser_getast(parser, ast);
+}
+
+int main(int argc, char *argv[])
+{
+    lexer_t *lexer = lexer_new(stdin);
+    parser_t *parser = parser_new(lexer);
+    compiler_t *comp = compiler_new();
+    ast_t *ast = NULL;
+    ast_kind_t kind = prompt(parser, &ast);
+    while (kind != AST_INVALID) {
+        if (kind == AST_END_OF_FILE)
+            break;
+        fprintf(stdout, "AST > ");
+        fflush(stdout);
+        ast_print(ast, stdout);
+        fputc('\n', stdout);
+
+        ins_t *ins = ins_new();
+        if (compiler_do(comp, ast, ins) == 1) {
+            fprintf(stdout, "BYTECODE > ");
+            fflush(stdout);
+            ins_print(ins, stdout);
+        } else
+            fprintf(stdout, "Compiler error: %s\n", comp->error->text);
+        kind = prompt(parser, &ast);
+    }
+    return 0;
+}
